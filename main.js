a9db7206f0ae8dda50a6deaf699f5ba5fcef16ec
Kaio Henrique <kaiohenriquemalaquias@gmail.com>
2026-04-09 15:15:07 -0300

    Update 4.0

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    backgroundColor: '#050816',
+    icon: path.join(__dirname, 'assets', 'logo_bigger_transparent.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
